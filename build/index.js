00c181f4f728c3503679f6b15a0ba7b34c87db84
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-08-22 21:10:55 +0530

    Added the JavaScript Functionality for Filter.

--- build/index.js
+++ build/index.js
@@ -7,13 +7,20 @@ var addBtn = document.querySelector(".container__left-button");
 var taskDescription = document.querySelector(".container__left-textarea");
 var taskContainer = document.querySelector(".container__right-lower");
 var categoryContainer = document.querySelector(".container__left-categorycontainer");
+var searchEl = document.querySelector(".container__right-titleinput");
+var priorityEl = document.querySelector(".container__right-priority");
 // console.log(taskTitle.value);
+var allTaskArr = [];
 function createTask() {
     var newTask = document.createElement("div");
     var checkboxDiv = document.createElement("div");
     var descriptionDiv = document.createElement("div");
     var checkbox = document.createElement("input");
     var deleteDiv = document.createElement("div");
+    var dueDiv = document.createElement("div");
+    var borderDiv1 = document.createElement("div");
+    var borderDiv2 = document.createElement("div");
+    var catDiv = document.createElement("div");
     var titleText = document.createElement("h3");
     var dueDate = document.createElement("p");
     var dueTime = document.createElement("p");
@@ -73,46 +80,139 @@ function createTask() {
     // });
     checkboxDiv.classList.add("container__right-checkboxDiv");
     deleteDiv.classList.add("container__right-deleteDiv");
+    dueDiv.classList.add("container__right-dueDiv");
     descriptionDiv.classList.add("container__right-descriptionDiv");
     checkbox.classList.add("container__right-checkboxEl");
     newTask.classList.add("container__right-taskscontainer");
     checkbox.type = "checkbox";
     checkboxDiv.appendChild(checkbox);
     titleText.textContent = taskTitle.value;
+    titleText.classList.add("container__right-tasktitle");
     descriptionDiv.appendChild(titleText);
     newTask.appendChild(checkboxDiv);
     dueDate.textContent = taskDueDate.value;
     var date = new Date(taskDueDate.value);
-    var day = date.toLocaleString('en-IN', { weekday: 'long' });
+    var day = date.toLocaleString('en-IN', { weekday: 'short' });
     var dayNo = date.getDate();
-    var month = date.toLocaleString('en-IN', { month: 'long' });
+    var month = date.toLocaleString('en-IN', { month: 'short' });
     var year = date.getFullYear();
     dueDate.textContent = "".concat(day, " ").concat(month, " ").concat(dayNo, " ").concat(year);
-    descriptionDiv.appendChild(dueDate);
+    dueDiv.appendChild(dueDate);
+    borderDiv1.classList.add("container__right-borderDiv");
+    dueDiv.appendChild(borderDiv1);
     dueTime.textContent = "12:10 in the Afternoon";
-    descriptionDiv.appendChild(dueTime);
+    dueDiv.appendChild(dueTime);
+    borderDiv2.classList.add("container__right-borderDiv");
+    dueDiv.appendChild(borderDiv2);
+    // console.log("Printing TaksPriority",taskPriority.value);
     if (taskPriority.value !== "") {
         taskPriorityElement.textContent = taskPriority.value;
     }
-    descriptionDiv.appendChild(taskPriorityElement);
-    if (taskCategory.value !== "") {
-        taskCategoryElement.textContent = taskCategory.value;
+    if (taskPriority.value === "High-Priority") {
+        // console.log("Hello Dheeraj");
+        dueDiv.classList.add("container__right-highPriority");
+        borderDiv1.classList.add("container__right-borderDivH");
+        borderDiv2.classList.add("container__right-borderDivH");
     }
-    descriptionDiv.appendChild(taskCategoryElement);
+    else if (taskPriority.value === "Medium-Priority") {
+        dueDiv.classList.add("container__right-mediumPriority");
+        borderDiv1.classList.add("container__right-borderDivM");
+        borderDiv2.classList.add("container__right-borderDivM");
+    }
+    else {
+        dueDiv.classList.add("container__right-lowPriority");
+        borderDiv1.classList.add("container__right-borderDivL");
+        borderDiv2.classList.add("container__right-borderDivL");
+    }
+    dueDiv.appendChild(taskPriorityElement);
+    descriptionDiv.appendChild(dueDiv);
+    // if(taskCategory.value !== ""){
+    //     taskCategoryElement.textContent = taskCategory.value;
+    // }
+    // descriptionDiv.appendChild(taskCategoryElement);
+    var selectedCategories = categoryContainer.querySelectorAll(" .container__left-categoryDiv div");
+    selectedCategories.forEach(function (categoryElement) {
+        var category = categoryElement.cloneNode(true);
+        category.classList.add("container__right-category");
+        catDiv.appendChild(category);
+    });
+    catDiv.classList.add("container__right-catDiv");
+    descriptionDiv.appendChild(catDiv);
     taskDescriptionElement.textContent = taskDescription.value;
     descriptionDiv.appendChild(taskDescriptionElement);
     newTask.appendChild(descriptionDiv);
-    deleteDiv.innerHTML = "<i class=\"fa-solid fa-trash\"></i>";
+    deleteDiv.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>";
     newTask.appendChild(deleteDiv);
     taskContainer.appendChild(newTask);
+    var tasksObj = {
+        title: taskTitle.value,
+        dueDate: dueDate.textContent,
+        priority: taskPriority.value,
+        category: taskCategory.value,
+        description: taskDescription.value
+    };
+    allTaskArr.push(tasksObj);
+    var stringArr = JSON.stringify(allTaskArr);
+    localStorage.setItem("taskDetaisl", stringArr);
+    // tasksObj.title = titleText;
+    // taskTitle.value = "";
+    // taskDueDate.value = "";
+    // taskPriority.value = "";
+    // taskDescription.value = "";
+    // taskCategory.value = "";
+    // categoryContainer.innerHTML = "";
 }
+function filter(searchVal) {
+    var searchLower = searchVal.toLowerCase().trim();
+    var tasks = taskContainer.querySelectorAll(".container__right-taskscontainer");
+    tasks.forEach(function (task) {
+        var _a, _b;
+        var titleEl = task.querySelector(".container__right-tasktitle");
+        var descriptionEl = task.querySelector(".container__right-descriptionDiv > p:last-child");
+        if (titleEl || descriptionEl) {
+            var taskTitleLower = ((_a = titleEl.textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase().trim()) || "";
+            var taskDescriptionLower = ((_b = descriptionEl.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase().trim()) || "";
+            var match = taskTitleLower.includes(searchLower) || taskDescriptionLower.includes(searchLower);
+            var taskEl = task;
+            if (match) {
+                taskEl.style.display = "";
+            }
+            else {
+                taskEl.style.display = "none";
+            }
+        }
+    });
+}
+searchEl.addEventListener("input", function () {
+    var searchVal = searchEl.value;
+    filter(searchVal);
+});
+// priorityEl.addEventListener("change", () =>{
+//     createTask();
+// });
 taskCategory.addEventListener('change', function () {
-    var categoryDiv = document.createElement("div");
-    categoryDiv.textContent = taskCategory.value;
-    categoryContainer.appendChild(categoryDiv);
+    var categoryDiv = document.createElement('div');
+    var category = document.createElement('div');
+    var deleteBtn = document.createElement('button');
+    category.textContent = taskCategory.value;
+    categoryDiv.classList.add("container__left-categoryDiv");
+    deleteBtn.innerHTML = "<i class=\"fa-regular fa-circle-xmark\"></i>";
+    deleteBtn.classList.add("container__left-deleteBtn");
+    categoryDiv.appendChild(category);
+    categoryDiv.appendChild(deleteBtn);
+    if (categoryContainer.contains(category)) {
+        return;
+    }
+    else {
+        categoryContainer.appendChild(categoryDiv);
+    }
+    deleteBtn.addEventListener('click', function () {
+        categoryDiv.remove();
+    });
 });
 addBtn.addEventListener("click", function (e) {
     e.preventDefault();
     createTask();
+    console.log(allTaskArr);
 });
 // createTask();
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ let addBtn = document.querySelector(".container__left-button") as HTMLButtonElem
 let taskDescription = document.querySelector(".container__left-textarea") as HTMLTextAreaElement;
 let taskContainer = document.querySelector(".container__right-lower") as HTMLElement;
 let categoryContainer = document.querySelector(".container__left-categorycontainer") as HTMLElement;
+let searchEl = document.querySelector(".container__right-titleinput") as HTMLInputElement;
+let priorityEl = document.querySelector(".container__right-priority") as HTMLElement;
 // console.log(taskTitle.value);
+
+let allTaskArr: Array<any> = [];
 
 function createTask(){
     let newTask = document.createElement("div");
@@ -14,6 +18,10 @@ function createTask(){
     let descriptionDiv = document.createElement("div");
     let checkbox = document.createElement("input");
     let deleteDiv = document.createElement("div");
+    let dueDiv = document.createElement("div");
+    let borderDiv1 = document.createElement("div");
+    let borderDiv2 = document.createElement("div");
+    let catDiv = document.createElement("div");
     let titleText = document.createElement("h3");
     let dueDate = document.createElement("p");
     let dueTime = document.createElement("p");
@@ -82,6 +90,7 @@ function createTask(){
 
     checkboxDiv.classList.add("container__right-checkboxDiv");
     deleteDiv.classList.add("container__right-deleteDiv");
+    dueDiv.classList.add("container__right-dueDiv");
     descriptionDiv.classList.add("container__right-descriptionDiv");
     checkbox.classList.add("container__right-checkboxEl");
     newTask.classList.add("container__right-taskscontainer");
@@ -89,44 +98,148 @@ function createTask(){
     checkbox.type = "checkbox";
     checkboxDiv.appendChild(checkbox);
     titleText.textContent = taskTitle.value;
+    titleText.classList.add("container__right-tasktitle");
     descriptionDiv.appendChild(titleText);
     newTask.appendChild(checkboxDiv);
     dueDate.textContent = taskDueDate.value;
     let date = new Date(taskDueDate.value);
-    let day = date.toLocaleString('en-IN', { weekday: 'long' });
+    let day = date.toLocaleString('en-IN', { weekday: 'short' });
     let dayNo = date.getDate();
-    const month = date.toLocaleString('en-IN', { month: 'long' });
+    const month = date.toLocaleString('en-IN', { month: 'short' });
     let year = date.getFullYear();
     dueDate.textContent = `${day} ${month} ${dayNo} ${year}`;
-    descriptionDiv.appendChild(dueDate);
+    dueDiv.appendChild(dueDate);
+    borderDiv1.classList.add("container__right-borderDiv");
+    dueDiv.appendChild(borderDiv1);
     dueTime.textContent = `12:10 in the Afternoon`;
-    descriptionDiv.appendChild(dueTime);
+    dueDiv.appendChild(dueTime);
+    borderDiv2.classList.add("container__right-borderDiv");
+    dueDiv.appendChild(borderDiv2);
+    // console.log("Printing TaksPriority",taskPriority.value);
     if(taskPriority.value !== ""){
         taskPriorityElement.textContent = taskPriority.value;
     }
-    descriptionDiv.appendChild(taskPriorityElement);
-    if(taskCategory.value !== ""){
-        taskCategoryElement.textContent = taskCategory.value;
+    if(taskPriority.value === "High-Priority"){
+        // console.log("Hello Dheeraj");
+        dueDiv.classList.add("container__right-highPriority");
+        borderDiv1.classList.add("container__right-borderDivH");
+        borderDiv2.classList.add("container__right-borderDivH");
+    }else if(taskPriority.value === "Medium-Priority"){
+        dueDiv.classList.add("container__right-mediumPriority");
+        borderDiv1.classList.add("container__right-borderDivM");
+        borderDiv2.classList.add("container__right-borderDivM");
+    }else{
+        dueDiv.classList.add("container__right-lowPriority");
+        borderDiv1.classList.add("container__right-borderDivL");
+        borderDiv2.classList.add("container__right-borderDivL");
     }
-    descriptionDiv.appendChild(taskCategoryElement);
+    dueDiv.appendChild(taskPriorityElement);
+    descriptionDiv.appendChild(dueDiv);
+    // if(taskCategory.value !== ""){
+    //     taskCategoryElement.textContent = taskCategory.value;
+    // }
+    // descriptionDiv.appendChild(taskCategoryElement);
+    let selectedCategories = categoryContainer.querySelectorAll(" .container__left-categoryDiv div");
+    selectedCategories.forEach(categoryElement => {
+        let category = categoryElement.cloneNode(true) as HTMLElement;
+        category.classList.add("container__right-category");
+        catDiv.appendChild(category);
+    });
+    catDiv.classList.add("container__right-catDiv");
+    descriptionDiv.appendChild(catDiv);
     taskDescriptionElement.textContent = taskDescription.value;
     descriptionDiv.appendChild(taskDescriptionElement);
     newTask.appendChild(descriptionDiv);
-    deleteDiv.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deleteDiv.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
     newTask.appendChild(deleteDiv);
 
     taskContainer.appendChild(newTask);
+
+    let tasksObj: object = {
+        title: taskTitle.value,
+        dueDate: dueDate.textContent,
+        priority: taskPriority.value,
+        category: taskCategory.value,
+        description: taskDescription.value
+    };
+
+    allTaskArr.push(tasksObj);
+    let stringArr = JSON.stringify(allTaskArr);
+    localStorage.setItem("taskDetaisl", stringArr);
+
+    // tasksObj.title = titleText;
+
+    // taskTitle.value = "";
+    // taskDueDate.value = "";
+    // taskPriority.value = "";
+    // taskDescription.value = "";
+    // taskCategory.value = "";
+    // categoryContainer.innerHTML = "";
 }
 
+function filter(searchVal: string) {
+    const searchLower = searchVal.toLowerCase().trim();
+    const tasks = taskContainer.querySelectorAll(".container__right-taskscontainer");
+
+    tasks.forEach((task) => {
+        const titleEl = task.querySelector(".container__right-tasktitle") as HTMLElement;
+        const descriptionEl = task.querySelector(".container__right-descriptionDiv > p:last-child") as HTMLElement;
+
+        if (titleEl || descriptionEl) {
+            const taskTitleLower = titleEl.textContent?.toLowerCase().trim() || "";
+            const taskDescriptionLower = descriptionEl.textContent?.toLowerCase().trim() || "";
+
+            const match = taskTitleLower.includes(searchLower) || taskDescriptionLower.includes(searchLower);
+
+            const taskEl = task as HTMLElement;
+
+            if (match) {
+                taskEl.style.display = "";
+            } else {
+                taskEl.style.display = "none";
+            }
+        }
+    });
+}
+
+searchEl.addEventListener("input", () => {
+    const searchVal = searchEl.value;
+    filter(searchVal);
+});
+
+// priorityEl.addEventListener("change", () =>{
+//     createTask();
+// });
+
 taskCategory.addEventListener('change', () => {
-    let categoryDiv = document.createElement("div");
-    categoryDiv.textContent = taskCategory.value;
-    categoryContainer.appendChild(categoryDiv);
+    let categoryDiv = document.createElement('div');
+    let category = document.createElement('div');
+    let deleteBtn = document.createElement('button');
+
+    category.textContent = taskCategory.value;
+    categoryDiv.classList.add("container__left-categoryDiv");
+    deleteBtn.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+    deleteBtn.classList.add("container__left-deleteBtn");
+
+    categoryDiv.appendChild(category);
+    categoryDiv.appendChild(deleteBtn);
+
+    if(categoryContainer.contains(category)){
+        return;
+    }
+    else{
+        categoryContainer.appendChild(categoryDiv);
+    }
+
+    deleteBtn.addEventListener('click', () => {
+        categoryDiv.remove();
+    });
 });
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
     createTask();
+    console.log(allTaskArr);
 });
 
 // createTask();
